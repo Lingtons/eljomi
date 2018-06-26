ff9cb6dcf95c594f6a7136ef69af57362b2736aa
lingtons <austintappy@gmail.com>
2018-06-26 09:56:30 +0100

    structure updated

---
+++ database/migrations/2018_06_26_081349_create_customers_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email')->unique()->nullable();
+            $table->string('nickname')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('address')->nullable();
+            $table->date('dob')->nullable();
+            $table->enum('gender', ['Female', 'Male']);
+            $table->integer('type_id')->unsigned();
+            $table->timestamps();            
+            $table->foreign('type_id')->references('id')->on('types');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
